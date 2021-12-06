0cac2a6d352e3dd9f0376d45160b3f679abd8ea5
quangvuong666 <tinhnoibienkhoi@gmail.com>
2021-12-06 09:51:14 +0700

    vuong

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import DemoFunction from './Components/DemoComponent/DemoFunction';
+import DemoClass from './Components/DemoComponent/DemoClass';
+import CardProduct from './Components/DemoComponent/CardProduct';
+import BaiTapLayout1 from './Components/BaiTapLayout1/BaiTapLayout1';
+import Databinding from './Databinding/Databinding';
+
+function App() {
+  return (
+    <div className="App">
+        {/* <BaiTapLayout1 /> */}
+        <Databinding />
+    </div>
+  );
+}
+
+export default App;
